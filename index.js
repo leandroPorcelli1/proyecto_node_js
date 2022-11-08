477d05babfd5c1f325a8f659e1dd42b41618e59c
leandro porcelli <leandroporcelli657@gmail.com>
2022-11-08 16:59:35 -0300

    Intro-uno-js : primer ejercicio en JS.

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+let ciudad = 'cambridge';
+
+let num = 5;
+
+
+ciudad = num;
+
+console.log(ciudad);
+console.log(num);
+
+const nombre = 'leandro';
+
+const apellido = 'porcelli';
+
+nombreCompleto = `mi nombre es: ${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
