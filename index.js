9d2e89750a9030e20fea0d4a17f17c8c290c4ccb
leandro porcelli <leandroporcelli657@gmail.com>
2022-12-07 11:13:36 -0300

    ejercicio3: en este commit se realizaron los 3 puntos del ejercicio 3

---
+++ index.js
@@ -0,0 +1,45 @@
+import { heroes } from "./heroes.js";
+
+//PUNTO 1
+
+const getHeroeById = (id) => {
+  return heroes.find((heroe) => heroe.id === id);
+};
+
+const getHeroeByIdAsinc = (id) => {
+  return new Promise((resolve, reject) => {
+    const heroe = getHeroeById(id);
+    if (heroe) {
+      resolve(heroe);
+    } else {
+      reject("no se ha encontrado ningun heroe con ese id");
+    }
+  });
+};
+
+getHeroeByIdAsinc(2)
+  .then((heroe) => console.log(heroe))
+  .catch((err) => console.warn(err));
+
+// getHeroeByIdAsinc(10)
+//   .then((heroe) => console.log(heroe))
+//   .catch((err) => console.warn(err));
+
+//PUNTO2
+
+const peticion = fetch("https://api.github.com/users/manishmshiva");
+
+peticion
+  .then((respuesta) => respuesta.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.warn(err));
+
+//PUNTO3
+
+const getName = async () => {
+  const respuesta = await fetch("https://api.github.com/users/manishmshiva");
+  const data = await respuesta.json();
+  console.log(data.name);
+};
+
+getName();
